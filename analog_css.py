affa42ad031ee7e298567e706574e13df6af62bb
jammincoder <jammincoderguy96@gmail.com>
2022-01-19 14:07:59 -0500

    Program now checks for new CSS to generate upon quitting.

--- analog_css.py
+++ analog_css.py
@@ -10,25 +10,33 @@ print("[+] Watching files")
 while True:
     output_path = get_output_css_path()
     try:
-        files = find_files()
+        files = find_files() # Find all the files the user wants the program to track.
 
-        generated_classes = list()
+        generated_classes = list() # A list to store the generated CSS classes.
 
         for file in files:
-            classes = get_classes_from_file(file)
+            class_names = get_classes_from_file(file)  # Gets the CSS classes the user used in their markup.
 
-            for _class in classes:
+            # Loop over all the found class names and generate a CSS class with them.
+            for _class in class_names:
                 shorthand_class = ShorthandClass(_class)
                 generated_classes.append(shorthand_class.generate())
 
 
-        write_file(output_path, "")
+        write_file(output_path, "") # Delete the contents of the output file.
+
+        # Append all the generated classes to the output file
         for _class in generated_classes:
             append_to_file(output_path, _class)
         time.sleep(1)
 
     except KeyboardInterrupt:
-        print("\n[+] Stopped watching files")
+        # Make sure the program generates all the classes again before quitting.
+        print("[+] Generating classes one more time.")
+        for _class in generated_classes:
+            append_to_file(output_path, _class)
+
+        print("\n[+] Done. Stopped watching files")
         exit()
         
 
